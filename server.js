86e22878c949813f78f61a49ddb18e5ce800c30a
wendy <mizukilee@gmail.com>
2024-08-16 12:56:38 -0400

    intro to express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+const app = express(); 
+
+
+app.get("/", (req, res) => { 
+    console.log("Here")
+    res.sendStatus(500)
+    res.send("Hi")
+    
+}); 
+
+app.listen(3000); 
